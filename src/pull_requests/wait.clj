5c8b220e3e6e7cf4f10f57d694c9920dc01edd0f
Rafael Ferreira <rafa@rafasf.com>
2019-10-07 10:25:51 +0100

    Select relevant fields from full pull request

--- src/pull_requests/wait.clj
+++ src/pull_requests/wait.clj
@@ -1,6 +1,19 @@
 (ns pull-requests.wait
-  (:require [pull-requests.client :refer [fetch-all]]))
+  (:require [clojure.set :refer [rename-keys]]
+            [pull-requests.client :refer [fetch-all]]))
 
 (defn closed-pull-requests-for [owner repository]
   (fetch-all (str "https://api.github.com/repos/" owner "/" repository "/pulls")
              {"state" "closed"}))
+
+(defn gh-pr-fields [pr]
+  (select-keys pr [:number :title :created_at :closed_at]))
+
+(defn adjust-key-names [pr]
+  (rename-keys pr {:number :id}))
+
+(defn id-to-string [pr]
+  (update pr :id str))
+
+(defn gh-select-fields [prs]
+  (map (comp id-to-string adjust-key-names gh-pr-fields) prs))
--- test/pull_requests/wait_test.clj
+++ test/pull_requests/wait_test.clj
@@ -3,6 +3,13 @@
   (:require [clojure.test :refer :all]
             [pull-requests.wait :refer :all]))
 
+(def github-pull-requests
+  [{:number 123
+    :title "some title"
+    :other-field "not important"
+    :created_at "2011-04-10T20:09:31Z"
+    :closed_at "2011-04-10T20:09:31Z"}])
+
 (deftest fetch-closed-pull-requests-for-given-repository
   (with-fake-http [{:url "https://api.github.com/repos/the-owner/a-repo/pulls"
                     :query-params {"state" "closed"}
@@ -12,3 +19,10 @@
     (let [prs (closed-pull-requests-for "the-owner" "a-repo")]
       (is (= [{:number 123}]
              prs)))))
+
+(deftest returns-relevant-fields
+  (is (= [{:id "123"
+           :title "some title"
+           :created_at "2011-04-10T20:09:31Z"
+           :closed_at "2011-04-10T20:09:31Z"}]
+         (gh-select-fields github-pull-requests))))
