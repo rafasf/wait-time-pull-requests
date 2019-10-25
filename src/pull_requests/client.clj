9e7b2c7e76209a0fa57d9ae12fe1bcb2dced9a0b
Rafael Ferreira <rafa@rafasf.com>
2019-10-25 11:53:53 +0100

    Create either public or authenticated provider
    Expanding the concept of the provider to support basic auth.

--- src/pull_requests/client.clj
+++ src/pull_requests/client.clj
@@ -9,10 +9,14 @@
 (defn next-page-in [headers]
   (-> headers parse-links (get-in [:links :next]) :href))
 
+(defn provider-for
+  ([url] {:url url})
+  ([url username secret] {:url url :basic-auth [username secret]}))
+
 (defn fetch-all
   ([provider query-params] (fetch-all provider query-params []))
   ([provider query-params prs] (let [{:keys [headers body]} @(client/get (provider :url) {:query-params query-params})]
-                            (let [next-page-url (next-page-in headers)]
-                              (if-not next-page-url
-                                (concat prs (parse-to-map body))
-                                (recur (assoc provider :url next-page-url) {} (concat prs (parse-to-map body))))))))
+                                 (let [next-page-url (next-page-in headers)]
+                                   (if-not next-page-url
+                                     (concat prs (parse-to-map body))
+                                     (recur (assoc provider :url next-page-url) {} (concat prs (parse-to-map body))))))))
--- test/pull_requests/client_test.clj
+++ test/pull_requests/client_test.clj
@@ -5,20 +5,29 @@
 
 (def paged-gh-responses
   [{:url "https://gh2.com/pulls?page=2"
-                    :method :get}
-                   {:status 200
-                    :body "[{\"number\": 3}]"}
+    :method :get}
+   {:status 200
+    :body "[{\"number\": 3}]"}
 
-                   {:url "https://api.github.com/the-owner/a-repo/pulls"
-                    :query-params {"state" "something"}
-                    :method :get}
-                   {:status 200
-                    :headers {:link "<https://gh2.com/pulls?page=2>; rel=\"next\", <https://gh.com/pulls?page=2>; rel=\"last\""}
-                    :body "[{\"number\": 1}, {\"number\": 2}]"}])
+   {:url "https://api.github.com/the-owner/a-repo/pulls"
+    :query-params {"state" "something"}
+    :method :get}
+   {:status 200
+    :headers {:link "<https://gh2.com/pulls?page=2>; rel=\"next\", <https://gh.com/pulls?page=2>; rel=\"last\""}
+    :body "[{\"number\": 1}, {\"number\": 2}]"}])
 
 (deftest fetch-all-pages
   (with-fake-http paged-gh-responses
-    (let [destination {:url "https://api.github.com/the-owner/a-repo/pulls"}
-          prs (fetch-all destination {"state" "something"})]
+    (let [provider {:url "https://api.github.com/the-owner/a-repo/pulls"}
+          prs (fetch-all provider {"state" "something"})]
       (is (= [{:number 1} {:number 2} {:number 3}]
              prs)))))
+
+(deftest provider-creation
+  (testing "non-authenticated provider"
+    (is (= {:url "https://someprovider.com"}
+           (provider-for "https://someprovider.com"))))
+
+  (testing "authenticated provider"
+    (is (= {:url "https://someprovider.com" :basic-auth ["bob" "secret"]}
+           (provider-for "https://someprovider.com" "bob" "secret")))))
